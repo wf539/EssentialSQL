314138218913ffe01de3551ae6e83198c9213bd7
William Farmer <willetfarm@yahoo.com>
2020-03-13 23:23:19 +0000

    Add T-SQL characters, formatting

--- MyFirstEssentialSQLQuery.sql
+++ MyFirstEssentialSQLQuery.sql
@@ -1,11 +1,41 @@
-SELECT Person.Person.FirstName, Person.Person.LastName
-FROM Person.Person
-ORDER BY LastName
+--Add T-SQLfeatures 2020-03-13
 
-SELECT		FirstName,
-			LastName
-FROM		Person.Person
---ORDER BY	Lastname
+USE [AdventureWorks2017];	--Add T-SQLfeatures 2020-03-13
+GO							--Add T-SQLfeatures 2020-03-13
 
-SELECT *
-FROM	Person.Person
+SELECT 
+	[Person].[Person].[FirstName]
+	, [Person].[Person].[LastName]
+FROM 
+	[Person].[Person]
+ORDER BY
+	[LastName];
+
+SELECT
+	[FirstName]
+	, [LastName]
+FROM
+	[Person].[Person];
+--ORDER BY
+	--[Lastname]
+
+--SELECT *
+--FROM
+--	[Person].[Person];
+
+SELECT
+	[BusinessEntityID]
+	, [PersonType]
+	, [NameStyle]
+	, [Title]
+	, [FirstName]
+	, [MiddleName]
+	, [LastName]
+	, [Suffix]
+	, [EmailPromotion]
+	, [AdditionalContactInfo]
+	, [Demographics]
+	, [rowguid]
+	, [ModifiedDate]
+FROM
+	[Person].[Person];
